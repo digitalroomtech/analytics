378ac7b9b2ccd2d8f1b981777baf793cf05877aa
Kristopher Jafeth <43774719+kristopherjafeth@users.noreply.github.com>
2023-11-22 15:32:56 -0400

    add analytics controller

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import { authenticate, createEvent } from './analyticsController';
 
 const app = express();
 const port = 3002;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola mundo!');
+app.use(express.json());
+
+app.get('/authenticate', authenticate);
+
+app.post('/createEvent', async (req, res) => {
+  const { name, uuid } = req.body;
+  await createEvent(req, res);
 });
 
 app.listen(port, () => {
