e705c17bdc41bee8edd7330730c39629dbdedbd3
LuisEulacio <luiseulacio1255@gmail.com>
2024-01-22 20:13:24 -0400

    create index

--- src/scripts/create-index.ts
+++ src/scripts/create-index.ts
@@ -2,7 +2,7 @@ import { analyticsCollection } from '../utils/mongodb';
 
 const main = async () => {
   const analytics = await analyticsCollection();
-  await analytics.createIndex('uuid');
+  await analytics.createIndex({ uuid: 1 });
   console.log('final');
 };
 
