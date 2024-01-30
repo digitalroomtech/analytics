212921dba26654653f6a3ff921682894b6066bef
LuisEulacio <luiseulacio1255@gmail.com>
2024-01-30 08:09:20 -0400

    many changes

--- src/modules/metrics/metrics.query.resolvers.ts
+++ src/modules/metrics/metrics.query.resolvers.ts
@@ -9,7 +9,7 @@ import {
 const getClickedReport = async (parent: any, args: any, context: any) => {
   try {
     // eslint-disable-next-line prefer-const
-    let { events, from, to, tenantId } = args.variables;
+    let { events, from, to, tenantId } = args.where;
     from = new Date(new Date(from).setHours(0, 0, 0));
     to = new Date(new Date(to).setHours(23, 59, 59));
 
@@ -33,7 +33,7 @@ const getClickedReport = async (parent: any, args: any, context: any) => {
 const getRegisteredUserReport = async (parent: any, args: any, context: any) => {
   try {
     // eslint-disable-next-line prefer-const
-    let { from, to, tenantId } = args.variables;
+    let { from, to, tenantId } = args.where;
     from = new Date(new Date(from).setHours(0, 0, 0));
     to = new Date(new Date(to).setHours(23, 59, 59));
 
@@ -77,10 +77,56 @@ const getRegisteredUserReport = async (parent: any, args: any, context: any) => 
   }
 };
 
+const visitPageByUsers = async (parent: any, args: any, context: any) => {
+  try {
+    // eslint-disable-next-line prefer-const
+    let { from, to, tenantId } = args.where;
+    from = new Date(new Date(from).setHours(0, 0, 0));
+    to = new Date(new Date(to).setHours(23, 59, 59));
+
+    const response = await RegisteredUserMetricsModel.aggregate([
+      {
+        $match: {
+          name: { $eq: 'page_visit' },
+          created_at: { $gte: from, $lt: to },
+          // tenant_id: tenantId,
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          total_users_registered: {
+            $sum: {
+              $cond: {
+                if: { $ne: ['$user_id', 0] },
+                then: 1,
+                else: 0,
+              },
+            },
+          },
+          total_user_anonymous: {
+            $sum: {
+              $cond: {
+                if: { $eq: ['$user_id', 0] },
+                then: 1,
+                else: 0,
+              },
+            },
+          },
+        },
+      },
+    ]);
+    return response[0];
+  } catch (error) {
+    console.error('Error Get Registered User Report', error);
+    return [];
+  }
+};
+
 const getHeatMapReport = async (parent: any, args: any, context: any) => {
   try {
     // eslint-disable-next-line prefer-const
-    let { from, to, event, tenantId } = args.variables;
+    let { from, to, event, tenantId } = args.where;
     from = new Date(new Date(from).setHours(0, 0, 0));
     to = new Date(new Date(to).setHours(23, 59, 59));
 
@@ -120,7 +166,7 @@ const getHeatMapReport = async (parent: any, args: any, context: any) => {
 const getUrlVisitReport = async (parent: any, args: any, context: any) => {
   try {
     // eslint-disable-next-line prefer-const
-    let { from, to, tenantId } = args.variables;
+    let { from, to, tenantId } = args.where;
     from = new Date(new Date(from).setHours(0, 0, 0));
     to = new Date(new Date(to).setHours(23, 59, 59));
 
@@ -191,4 +237,5 @@ export const metricsQueryResolvers = {
   getHeatMapReport,
   getUrlVisitReport,
   getUsersByMonthReport,
+  visitPageByUsers,
 };
