25ee7b15196f71d527ac0688262f876bc52a0c46
Luis Eulacio <luiseulacio1255@gmail.com>
2024-05-20 08:59:19 -0400

    fix url invalid

--- src/middlewares/check-origin.middleware.ts
+++ src/middlewares/check-origin.middleware.ts
@@ -1,42 +1,38 @@
 import { Request as ExpressRequest, Response, NextFunction } from 'express';
 import { TenantModel } from '../modules/tenant/tenant.models';
-import { TenantStatuses } from '../modules/tenant/tenant.types';
+import { ITenant, TenantStatuses } from '../modules/tenant/tenant.types';
 
 interface Request extends ExpressRequest {
   tenant_id?: number;
 }
 
 export const checkOriginMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-  const sessionOrigin = req.headers['analytics-session-origin'];
-
-
-  console.log({
-    sessionOrigin,
-    header:req.headers.origin
-  })
-
-  const url = new URL((sessionOrigin || req.headers.origin) as string);
-  let tenant = undefined;
-
 
   try {
+    const sessionOrigin = req.headers['analytics-session-origin'];
+
+    const url = new URL((sessionOrigin || req.headers.origin) as string);
+    let tenant: ITenant | undefined | null = undefined;
+
     tenant = await TenantModel.findOne({
       allowedUrls: url.origin || '',
       status: TenantStatuses.ACTIVE,
     });
+
+    if (!tenant) {
+      return res.status(400).send({
+        message: `Invalid origin: ${url.origin} ${sessionOrigin} ${
+          req.headers.origin
+        } ${JSON.stringify(url)}`,
+      });
+    }
+    req.body.tenant_id = tenant._id;
+    req.body.originUrl = url.href;
+
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
   }
 
-  if (!tenant) {
-    return res.status(400).send({
-      message: `Invalid origin: ${url.origin} ${sessionOrigin} ${
-        req.headers.origin
-      } ${JSON.stringify(url)}`,
-    });
-  }
-  req.body.tenant_id = tenant._id;
-  req.body.originUrl = url.href;
 
   return next();
 };
