5ef6daaf1b8044e7de3bcc940ef1f5a11f0d8de5
Luis Eulacio <luiseulacio1255@gmail.com>
2024-08-29 13:31:06 -0400

    add new field

--- src/modules/user/user.query.resolvers.ts
+++ src/modules/user/user.query.resolvers.ts
@@ -83,6 +83,7 @@ const userEvents = async (
         id: '$_id',
         name: 1,
         created_at: 1,
+        user_id: 1,
         event_meta: {
           $map: {
             input: '$event_meta',
